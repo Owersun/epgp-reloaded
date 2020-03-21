18e76b1b4dbe58bf7471aa4ad74cf361e0856a9e
oversun <shiva@mail.ru>
2020-03-21 22:32:53 +0100

    Make communication messages work as a brief skeleton

--- Communication.lua
+++ Communication.lua
@@ -1,12 +1,20 @@
 local EPGPR = EPGPR
 
+EPGPR.Messages = {
+    ENCOUNTERWON = 1,
+    ITEMDISTRIBUTED = 2,
+}
+
 -- When a message is received
-function EPGPR:OnCommReceived(payload)
-    local message = self:Deserialize(payload)
-    self:Print(message.type)
+function EPGPR:OnCommReceived(prefix, message, channel, sender)
+    if sender == UnitName("player") then return end -- ignore messages from yourself
+    local result, payload = self:Deserialize(message)
+    if result then
+        self:Print(prefix, payload[1], channel, sender)
+    end
 end
 
 -- Send message to through the channel
-function EPGPR:CommSend(payload, channel)
-    self:SendCommMessage(self.const.CommunicationPrefix, self:Serialaze(payload), channel)
+function EPGPR:CommSend(message, payload, channel)
+    self:SendCommMessage(self.Const.CommunicationPrefix, self:Serialize({message, payload}), channel)
 end
