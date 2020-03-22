1789eefd37ba17ef1d0568a20308d708e141a1af
oversun <shiva@mail.ru>
2020-03-22 15:23:11 +0100

    Add bid colors to chat announce and lootoverview announce window with bids

--- UI/Widget-LootOverviewItemAnnounce.lua
+++ UI/Widget-LootOverviewItemAnnounce.lua
@@ -1,10 +1,16 @@
-local EPGPR, RAID_CLASS_COLORS, AceGUI = EPGPR, RAID_CLASS_COLORS, EPGPR.Libs.AceGUI
+local EPGPR, RAID_CLASS_COLORS, ITEM_QUALITY_COLORS, AceGUI = EPGPR, RAID_CLASS_COLORS, ITEM_QUALITY_COLORS, EPGPR.Libs.AceGUI
 
 -- Compare function to sort widget children based on their GPRatio and PR
 local function sortFunc(widgetA, widgetB)
     local dataA, dataB = widgetA:GetUserData("data"), widgetB:GetUserData("data")
     -- first sort by GP bidder want to take the item (effectively his priority on the item), second by bidder priority rating
     if dataA.GP ~= dataB.GP then return dataA.GP > dataB.GP else return dataA.PR > dataB.PR end
+end
+
+-- return message decorated with wow color brakets depending on GPRatio. Full GPRatio of 1 is going to have color of epic item, which is 4 in color list
+local function paintBidMessage(message, GPRatio)
+    local color = ITEM_QUALITY_COLORS[math.floor(GPRatio * 4)] or 'ffffffff'
+    return "|c" .. color .. message .. "|r"
 end
 
 -- Item announce form that gathers all bids on the item and shows them as rows/buttons
@@ -31,6 +37,7 @@ EPGPR.UI.LootOverviewItemAnnounce = function(giveItemTo, itemGP)
         local GPRatio = message and config.messages[message] or (config.rollRatio or 1) -- GP ratio by which GP is going to be reduced for this bidder, e.g. 1/0.5/0.2/etc.
         local GP = math.floor(GPRatio * itemGP) -- actual GP, bidder is going to get this item for
         local classColor = RAID_CLASS_COLORS[player.class] and RAID_CLASS_COLORS[player.class].colorStr or 'ffffffff' -- bidder class color
+        local coloredMessage = paintBidMessage(message or "roll", GPRatio)
 
         -- add another bidder to the list of bidders
         local button = AceGUI:Create("InteractiveLabel")
@@ -38,7 +45,7 @@ EPGPR.UI.LootOverviewItemAnnounce = function(giveItemTo, itemGP)
         local fontFace = button.label:GetFont()
         button:SetFont(fontFace, 15)
         button.highlight:SetColorTexture(1, 1, 1, 0.1)
-        button:SetText(("|c%s%s|r (%s) %s (for %d GP), rating %.2f"):format(classColor, player.name, player.rank, (message or "roll"), GP, PR))
+        button:SetText(("|c%s%s|r (%s) %s (for %d GP), rating %.2f"):format(classColor, player.name, player.rank, coloredMessage, GP, PR))
         button:SetCallback("OnClick", function() giveItemTo(player.name, GP) end)
         button:SetUserData("data", { name = player.name, message = message, GP = GP, PR = PR })
         widget:AddChild(button)
@@ -46,7 +53,7 @@ EPGPR.UI.LootOverviewItemAnnounce = function(giveItemTo, itemGP)
         -- resort list of bidders (widget children) in place
         table.sort(widget.children, sortFunc)
         widget:DoLayout()
-        EPGPR:ChatBidPlaced(player.name, message or "roll", PR)
+        EPGPR:ChatBidPlaced(player.name, coloredMessage, PR)
     end
 
     LootOverviewItemAnnounce:SetCallback("Bid", bid)
