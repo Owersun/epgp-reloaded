30744ea923f0b4cb430ae537f530052afc5d2fe1
oversun <shiva@mail.ru>
2020-03-22 15:07:46 +0100

    Protect communication channel from calling any function on self, and create a proxy with only exposed functions list

--- Communication.lua
+++ Communication.lua
@@ -1,10 +1,16 @@
 local EPGPR, UnitName = EPGPR, UnitName
 
+-- this is a "safe-list" of functions exposed to be externally called.
+-- they are proxied through this table, to prevent exploits by allowing to call functions like EPGPR:GuildChangeMemeberEPGP()
+local communitaction = {
+    SaveHistoryRow = function(...) EPGPR:SaveHistoryRow(...) end
+}
+
 -- When a message is received, try to call the method with arguments
 function EPGPR:OnCommReceived(prefix, message, _, sender)
     if sender == UnitName("player") or prefix ~= self.Const.CommunicationPrefix then return end -- ignore messages not from the app or from yourself
     local result, method, arguments = self:Deserialize(message)
-    if result then pcall(self[method], self, unpack(arguments)) end
+    if result then pcall(communitaction[method], unpack(arguments)) end
 end
 
 -- Effectively this is RPC to apps that other guild members run. It's going to call the method on self with given arguments
